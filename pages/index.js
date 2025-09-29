40ec121612e636a4a72c147f24372979a434c4a0
PremiumStudios <tyrengatlin@theofficialpremium.com>
2025-09-29 10:48:18 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+ return (
+   <div style={{textAlign: "center", marginTop: "50px"}}>
+     <h1>🚀 Premium Sessions is Live!</h1>
+     <p>If you see this, your app is working on Vercel.</p>
+   </div>
+ )
+}
