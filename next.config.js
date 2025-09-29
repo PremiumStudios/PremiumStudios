88dffec9600d50f3c553e70b504c63c836a0e8ef
PremiumStudios <tyrengatlin@theofficialpremium.com>
2025-09-29 11:58:11 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { 
-    unoptimized: true,
-    domains: ['images.pexels.com', 'via.placeholder.com']
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', '*.vercel.app', '*.netlify.app']
-    }
-  }
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
